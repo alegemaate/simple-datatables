73c72533d1277ac0c650e1ced77dc900940014c7
Allan Legemaate <alegemaate@gmail.com>
2022-03-07 22:21:14 -0500

    Fix dev and build environment (#10)
    * Fix dev and build environment
    
    * Uncomment code
    
    * Remove test command from check workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
+  mode: "none",
   entry: "./examples/router/index.tsx",
-  mode: "development",
   context: __dirname,
   output: {
     filename: "bundle.js",
+    path: path.join(__dirname, "build"),
+    clean: true,
   },
   devtool: "inline-source-map",
   devServer: {
     port: 5050,
     historyApiFallback: true,
+    static: "./build",
   },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.m?js(x?)$/,
         exclude: /node_modules/,
         use: {
           loader: "swc-loader",
@@ -31,12 +35,21 @@ module.exports = {
       },
     ],
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./examples/index.html",
     }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
   ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      components: path.resolve(__dirname, "src/components"),
+      hooks: path.resolve(__dirname, "src/hooks"),
+      types: path.resolve(__dirname, "src/types"),
+      utils: path.resolve(__dirname, "src/utils"),
+    },
+  },
 };
